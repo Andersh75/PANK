5fae83513bf63c4c80a6cb61643615c344340896
Andersh75 <andershellstrom@Anderss-MBP.lan>
2015-12-08 19:24:30 +0100

    Initial Commit

---
+++ PANK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PANK
+//
+//  Created by Anders Hellström on 2015-12-08.
+//  Copyright (c) 2015 Anders Hellström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
